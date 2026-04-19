ec1337a63c70ec7a1a1043f32f2780d23deed40d
PotatoHyena <hal00l90900@gmail.com>
2026-04-20 00:48:05 +0300

    index

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,18 +2,19 @@ import readlineSync from 'readline-sync';
 
 const numberOfRounds = 3;
 
-const runGameEngine = (gameDescription, getQuestionResult) => {
+const runGameEngine = (gameDescription, getGameData) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(gameDescription);
 
-  for (let i = 0; i < numberOfRounds; i += 1) {
-    const [question, correctAnswer] = getQuestionResult();
+ for (let i = 0; i < numberOfRounds; i += 1) {
+    const [question, correctAnswer] = getGameData();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
+
     if (answer !== correctAnswer) {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
