57bb97ac5e33af0068a4aa70b31e6f2142d70800
PotatoHyena <hal00l90900@gmail.com>
2026-04-20 02:00:52 +0300

    idk how

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,17 +1,11 @@
 import getRandomNum from '../utils/randomNum.js';
 import runGameEngine from '../utils/index.js';
 
-const settings = {
-  minNum: 0,
-  maxNum: 100,
-};
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-const numIsEven = (num) => num % 2 === 0;
 
 const getGameData = () => {
-  const question = getRandomNum(settings.minNum, settings.maxNum);
-  const questionNumIsEven = numIsEven(question);
-  const correctAnswer = questionNumIsEven ? 'yes' : 'no';
+  const question = getRandomNum(0, 100);
+  const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,14 @@
 import readlineSync from 'readline-sync';
 
-const numberOfRounds = 3;
+const roundsCount = 3;
 
 const runGameEngine = (gameDescription, getGameData) => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(gameDescription);
 
-  for (let i = 0; i < numberOfRounds; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const [question, correctAnswer] = getGameData();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
