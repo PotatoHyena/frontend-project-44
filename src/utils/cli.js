7fd846b76bd928a8730467c0dc6a5adbc94b6551
PotatoHyena <hal00l90900@gmail.com>
2026-04-19 22:39:43 +0300

    hopeful fix

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 const greet = () => {
     console.log("Welcome to the Brain Games!");
-    const name = readlineSync.question('May I have your name?\n', {
+    const name = readlineSync.question('May I have your name?', {
         defaultInput: 'John',
     });
     console.log('Hello,${name}!');
