4ae2de1413e611f79750de53152b657cf5b8ad7a
PotatoHyena <hal00l90900@gmail.com>
2026-04-20 02:04:47 +0300

    asdadqawd

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
 const roundsCount = 3;
 
 const runGameEngine = (gameDescription, getGameData) => {
-  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(gameDescription);
